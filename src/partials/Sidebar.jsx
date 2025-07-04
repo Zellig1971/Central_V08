f5ac91974032cde25d93131740f081bf4b06e8f2
Paul Roman <pjroman71@gmail.com>
2025-07-04 20:30:45 +0200

    Updated Sidebar.jsx

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -62,7 +62,7 @@ function Sidebar({
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-gray-800 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-gray-800 dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-64'
         }`}
       >
@@ -93,7 +93,7 @@ function Sidebar({
         <div className="space-y-8">
           {/* Pages group */}
           <div>
-            <h3 className="text-xs uppercase text-gray-500 font-semibold pl-3">
+            <h3 className="text-xs uppercase text-gray-500 dark:text-gray-500 font-semibold pl-3">
               <span className="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">
                 •••
               </span>
@@ -107,8 +107,8 @@ function Sidebar({
                     <>
                       <a
                         href="#0"
-                        className={`block text-gray-200 truncate transition duration-150 ${
-                          (pathname === '/' || pathname.includes('dashboard')) ? 'hover:text-gray-200' : 'hover:text-white'
+                        className={`block text-gray-800 dark:text-gray-200 truncate transition duration-150 ${
+                          (pathname === '/' || pathname.includes('dashboard')) ? 'hover:text-gray-900 dark:hover:text-gray-200' : 'hover:text-gray-900 dark:hover:text-white'
                         }`}
                         onClick={(e) => {
                           e.preventDefault();
@@ -120,19 +120,19 @@ function Sidebar({
                             <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                               <path
                                 className={`fill-current ${
-                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-500' : 'text-gray-400'
+                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400'
                                 }`}
                                 d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z"
                               />
                               <path
                                 className={`fill-current ${
-                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-600' : 'text-gray-600'
+                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-600' : 'text-gray-600 dark:text-gray-600'
                                 }`}
                                 d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z"
                               />
                               <path
                                 className={`fill-current ${
-                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-200' : 'text-gray-400'
+                                  (pathname === '/' || pathname.includes('dashboard')) ? 'text-violet-200' : 'text-gray-400 dark:text-gray-400'
                                 }`}
                                 d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z"
                               />
@@ -143,7 +143,7 @@ function Sidebar({
                           </div>
                           {/* Icon */}
                           <div className="flex shrink-0 ml-2">
-                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
+                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
                               <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                             </svg>
                           </div>
@@ -156,7 +156,7 @@ function Sidebar({
                               end
                               to="/"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -169,7 +169,7 @@ function Sidebar({
                               end
                               to="/dashboard/analytics"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -182,7 +182,7 @@ function Sidebar({
                               end
                               to="/dashboard/fintech"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -206,8 +206,8 @@ function Sidebar({
                     <>
                       <a
                         href="#0"
-                        className={`block text-gray-200 truncate transition duration-150 ${
-                          pathname.includes('finance') ? 'hover:text-gray-200' : 'hover:text-white'
+                        className={`block text-gray-800 dark:text-gray-200 truncate transition duration-150 ${
+                          pathname.includes('finance') ? 'hover:text-gray-900 dark:hover:text-gray-200' : 'hover:text-gray-900 dark:hover:text-white'
                         }`}
                         onClick={(e) => {
                           e.preventDefault();
@@ -218,15 +218,15 @@ function Sidebar({
                           <div className="flex items-center">
                             <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                               <path
-                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-300' : 'text-gray-400'}`}
+                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-300' : 'text-gray-400 dark:text-gray-400'}`}
                                 d="M13 6.068a6.035 6.035 0 0 1 4.932 4.933H24c-.486-5.846-5.154-10.515-11-11v6.067Z"
                               />
                               <path
-                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-500' : 'text-gray-700'}`}
+                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-500' : 'text-gray-700 dark:text-gray-700'}`}
                                 d="M18.007 13c-.474 2.833-2.919 5-5.864 5a5.888 5.888 0 0 1-3.694-1.304L4 20.731C6.131 22.752 8.992 24 12.143 24c6.232 0 11.35-4.851 11.857-11h-5.993Z"
                               />
                               <path
-                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-600' : 'text-gray-600'}`}
+                                className={`fill-current ${pathname.includes('finance') ? 'text-violet-600' : 'text-gray-600 dark:text-gray-600'}`}
                                 d="M6.939 15.007A5.861 5.861 0 0 1 6 11.829c0-2.937 2.167-5.376 5-5.85V0C4.85.507 0 5.614 0 11.83c0 2.695.922 5.174 2.456 7.17l4.483-3.993Z"
                               />
                             </svg>
@@ -236,7 +236,7 @@ function Sidebar({
                           </div>
                           {/* Icon */}
                           <div className="flex shrink-0 ml-2">
-                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
+                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
                               <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                             </svg>
                           </div>
@@ -249,7 +249,7 @@ function Sidebar({
                               end
                               to="/finance/cards"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -262,7 +262,7 @@ function Sidebar({
                               end
                               to="/finance/transactions"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -275,7 +275,7 @@ function Sidebar({
                               end
                               to="/finance/transaction-details"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -297,8 +297,8 @@ function Sidebar({
                     <>
                       <a
                         href="#0"
-                        className={`block text-gray-200 truncate transition duration-150 ${
-                          pathname.includes('tasks') ? 'hover:text-gray-200' : 'hover:text-white'
+                        className={`block text-gray-800 dark:text-gray-200 truncate transition duration-150 ${
+                          pathname.includes('tasks') ? 'hover:text-gray-900 dark:hover:text-gray-200' : 'hover:text-gray-900 dark:hover:text-white'
                         }`}
                         onClick={(e) => {
                           e.preventDefault();
@@ -309,15 +309,15 @@ function Sidebar({
                           <div className="flex items-center">
                             <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                               <path
-                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-500' : 'text-gray-600'}`}
+                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-500' : 'text-gray-600 dark:text-gray-600'}`}
                                 d="M8 1v2H3v19h18V3h-5V1h7v23H1V1z"
                               />
                               <path
-                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-500' : 'text-gray-600'}`}
+                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-500' : 'text-gray-600 dark:text-gray-600'}`}
                                 d="M1 1h22v23H1z"
                               />
                               <path
-                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-300' : 'text-gray-400'}`}
+                                className={`fill-current ${pathname.includes('tasks') ? 'text-violet-300' : 'text-gray-400 dark:text-gray-400'}`}
                                 d="M15 10.586L16.414 12 11 17.414 7.586 14 9 12.586l2 2zM5 0h14v4H5z"
                               />
                             </svg>
@@ -327,7 +327,7 @@ function Sidebar({
                           </div>
                           {/* Icon */}
                           <div className="flex shrink-0 ml-2">
-                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
+                            <svg className={`w-3 h-3 shrink-0 ml-1 fill-current text-gray-400 dark:text-gray-400 ${open && 'rotate-180'}`} viewBox="0 0 12 12">
                               <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                             </svg>
                           </div>
@@ -340,7 +340,7 @@ function Sidebar({
                               end
                               to="/tasks/kanban"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -353,7 +353,7 @@ function Sidebar({
                               end
                               to="/tasks/list"
                               className={({ isActive }) =>
-                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 hover:text-gray-200')
+                                'block transition duration-150 truncate ' + (isActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200')
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
@@ -373,15 +373,15 @@ function Sidebar({
                 <NavLink
                   end
                   to="/calendar"
-                  className={`block text-gray-200 truncate transition duration-150 ${
-                    pathname.includes('calendar') ? 'hover:text-gray-200' : 'hover:text-white'
+                  className={`block text-gray-800 dark:text-gray-200 truncate transition duration-150 ${
+                    pathname.includes('calendar') ? 'hover:text-gray-900 dark:hover:text-gray-200' : 'hover:text-gray-900 dark:hover:text-white'
                   }`}
                 >
                   <div className="flex items-center">
                     <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path className={`fill-current ${pathname.includes('calendar') ? 'text-violet-500' : 'text-gray-600'}`} d="M1 3h22v20H1z" />
+                      <path className={`fill-current ${pathname.includes('calendar') ? 'text-violet-500' : 'text-gray-600 dark:text-gray-600'}`} d="M1 3h22v20H1z" />
                       <path
-                        className={`fill-current ${pathname.includes('calendar') ? 'text-violet-300' : 'text-gray-400'}`}
+                        className={`fill-current ${pathname.includes('calendar') ? 'text-violet-300' : 'text-gray-400 dark:text-gray-400'}`}
                         d="M21 3h2v4H1V3h2V1h4v2h10V1h4v2Z"
                       />
                     </svg>
@@ -397,18 +397,18 @@ function Sidebar({
                 <NavLink
                   end
                   to="/campaigns"
-                  className={`block text-gray-200 truncate transition duration-150 ${
-                    pathname.includes('campaigns') ? 'hover:text-gray-200' : 'hover:text-white'
+                  className={`block text-gray-800 dark:text-gray-200 truncate transition duration-150 ${
+                    pathname.includes('campaigns') ? 'hover:text-gray-900 dark:hover:text-gray-200' : 'hover:text-gray-900 dark:hover:text-white'
                   }`}
                 >
                   <div className="flex items-center">
                     <svg className="shrink-0 h-6 w-6" viewBox="0 0 24 24">
                       <path
-                        className={`fill-current ${pathname.includes('campaigns') ? 'text-violet-500' : 'text-gray-600'}`}
+                        className={`fill-current ${pathname.includes('campaigns') ? 'text-violet-500' : 'text-gray-600 dark:text-gray-600'}`}
                         d="M20 7a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 0120 7zM4 23a.75.75 0 01-.75-.75 1.5 1.5 0 00-1.5-1.5.75.75 0 110-1.5 1.5 1.5 0 001.5-1.5.75.75 0 111.5 0 1.5 1.5 0 001.5 1.5.75.75 0 110 1.5 1.5 1.5 0 00-1.5 1.5A.75.75 0 014 23z"
                       />
                       <path
-                        className={`fill-current ${pathname.includes('campaigns') ? 'text-violet-300' : 'text-gray-400'}`}
+                        className={`fill-current ${pathname.includes('campaigns') ? 'text-violet-300' : 'text-gray-400 dark:text-gray-400'}`}
                         d="M14.5 7c4.695 0 8.5 3.805 8.5 8.5s-3.805 8.5-8.5 8.5-8.5-3.805-8.5-8.5 3.805-8.5 8.5-8.5z"
                       />
                     </svg>
