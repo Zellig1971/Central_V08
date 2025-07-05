fba79e298d7e8259a297519a233a647de846e0f6
Paul Roman <pjroman71@gmail.com>
2025-07-05 11:21:56 +0200

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ThemeProvider, { ThemeContext } from './utils/ThemeContext';
+import ThemeProvider from './utils/ThemeContext';
 import App from './App';
-
-const Root = () => {
-  const { currentTheme } = useContext(ThemeContext);
-
-  useEffect(() => {
-    if (currentTheme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [currentTheme]);
-
-  return <App />;
-};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
-        <Root />
+        <App />
       </ThemeProvider>
     </Router>
   </React.StrictMode>
